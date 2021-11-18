dca54040eafa4336af955985585d242280bd847f
Devin Bhatt <12704239+devinbhatt@users.noreply.github.com>
2021-11-18 10:08:58 -0800

    server: API is now optional, CLI flags for API and port

--- server/index.js
+++ server/index.js
@@ -1,40 +1,48 @@
 //Import libraries
 const express = require('express');
 const cors = require('cors');
-
-//Express instance
-const app = express();
-
-//Import config
-const cfg = require('./config.json');
+const { program } = require('commander');
 
 //Import sensor handling code
 const sensorReadings = require('./sensorReadings');
 
-//Create an array with all sensor names (and therefore endpoint names)
-const sensorEndpoints = [];
-sensorReadings.sensorArray.forEach(function(sensor) {
-    sensorEndpoints.push(sensor.name);
-})
+//Define and parse CLI flags
+program
+    .option('--enable-api', 'Publish sensor data to HTTP API', false)
+    .option('-p, --port <port>', 'Port for HTTP API. Default: 80', '80');
+program.parse(process.argv);
+const options = program.opts();
 
-//Generate API endpoints for each sensor (With CORS)
-sensorReadings.sensorArray.forEach(function(sensor) {
-    app.get("/"+sensor.name, cors(), function (req, res) {
-        //Read sensor when endpoint is accessed
-        res.json({
-            "name":sensor.name,
-            "temperature":sensor.cachedTemperature.toFixed(1),
-            "lastRead":sensor.lastRead
+//Create Express instance if API is enabled
+if (options.enableApi) {
+    //Express instance
+    const app = express();
+
+    //Create an array with all sensor names (and therefore endpoint names)
+    const sensorEndpoints = [];
+    sensorReadings.sensorArray.forEach(function(sensor) {
+        sensorEndpoints.push(sensor.name);
+    })
+
+    //Generate API endpoints for each sensor (With CORS)
+    sensorReadings.sensorArray.forEach(function(sensor) {
+        app.get("/"+sensor.name, cors(), function (req, res) {
+            //Read sensor when endpoint is accessed
+            res.json({
+                "name":sensor.name,
+                "temperature":sensor.cachedTemperature.toFixed(1),
+                "lastRead":sensor.lastRead
+            });
         });
     });
-});
 
-//Create an endpoint that lists all the currently avalible sensors
-app.get("/endpoints", cors(), function(req, res) {
-    res.json(sensorEndpoints);
-});
+    //Create an endpoint that lists all the currently avalible sensors
+    app.get("/endpoints", cors(), function(req, res) {
+        res.json(sensorEndpoints);
+    });
 
-//Listener
-app.listen(cfg.serverConfig.port, function(){
-    console.log('API listening on port ', cfg.serverConfig.port);
-});
+    //Listener
+    app.listen(options.port, function(){
+        console.log('API listening on port ', options.port);
+    });
+}
