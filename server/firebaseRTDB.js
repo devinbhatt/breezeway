404a24c89f6ddebf5dda50bfce9b916287524e12
Devin Bhatt <12704239+devinbhatt@users.noreply.github.com>
2021-11-20 16:21:09 -0800

    server: switched to firebase-admin

--- server/firebaseRTDB.js
+++ server/firebaseRTDB.js
@@ -1,14 +1,17 @@
 //Imports
-const { initializeApp } = require('firebase/app');
-const { getDatabase, ref, set, update } = require('firebase/database');
-const firebaseConfig = require('./config/firebaseConfig.json');
+const admin = require('firebase-admin');
+const firebaseConfig = require('./config/firebaseConfig.json')
 
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+
+admin.initializeApp({
+    credential: admin.credential.cert(firebaseConfig.key),
+    databaseURL: firebaseConfig.databaseURL
+});
+const db = admin.database();
 
 //Function to create empty sensor object from supplied sensor name
 function initalizeSensor(sensor) {
-    set(ref(db, sensor.name), {
+    db.ref(sensor.name).set({
         name: sensor.name,
         temperature: null,
         lastRead: null
@@ -17,7 +20,7 @@ function initalizeSensor(sensor) {
 
 //Function to update sensor in database with current values
 function updateSensor(sensor) {
-    update(ref(db, sensor.name), {
+    db.ref(sensor.name).update({
         temperature: sensor.cachedTemperature,
         lastRead: sensor.lastRead
     });
